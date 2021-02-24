b3216b2d714fecb02deb5964f0287370f04af715
Gerson Lamela <gersonlamela7@gmail.com>
2021-02-24 21:14:32 +0000

    Aula 3

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { app } from "./app";
+
+/**
+* GET => Busca
+* POST => Salvar
+* PUT => Alterar
+* DELETE => Deletar
+* PACTH => Alteração especifica
+*/
+
+
+
+app.listen(3333, () => console.log('Maquina top começou da trabalhar!'));
+
+
+
